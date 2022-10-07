343e5bac38766aa9bef2acb831fed592f8637726
Michal <michalnow777@interia.pl>
2022-10-07 23:16:42 +0200

    built menu on mobile devices - showing and hiding sitll to add

--- src/Elements/Home/Navbar.js
+++ src/Elements/Home/Navbar.js
@@ -3,15 +3,38 @@ import ReactDOM from 'react-dom/client';
 import '../../ElementsStyles/Navbar.css';
 import * as Scroll from 'react-scroll';
 import { Link } from 'react-scroll';
+import { useState } from 'react';
 
 class Navbar extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			classname: 'navShow'
+		}
+	}
 	render() {
-		
+		//Shows navigation
+		const showNavigation = (event) => {
+			const showNav = 'navShow';
 
+			return(this.setState({classname: showNav}));
+		}
+		//Hides navigation
+		const hideNavigation = (event) => {
+			const hideNav = 'navHide';
+
+			return(this.setState({classname: hideNav}));
+		}
 		return(
 			<div className="Navbar">
 				<div className="Navlogo">
 					<img src={require("../../Images/logomin.png")}/>
+				</div>
+
+				<div className='burgerContainer'>
+					<div className='burger topBun'></div>
+					<div className='burger meat'></div>
+					<div className='burger bottomBun'></div>
 				</div>
 				<nav>
 					<Link activeClass="active" smooth spy to="appointment">
