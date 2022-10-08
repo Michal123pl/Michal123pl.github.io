3c41806c5b6e9e38127111dd1d5d69ff38c05e3f
Michal <michalnow777@interia.pl>
2022-10-08 22:18:36 +0200

    finished navbar with showing an hidding icon and functionallity

--- src/Elements/Home/Navbar.js
+++ src/Elements/Home/Navbar.js
@@ -9,21 +9,27 @@ class Navbar extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			classname: 'navShow'
+			navclass: 'navHide',
+			burgericonclass: 'burgerContainerShown',
+			xiconclass: 'xIconHidden'
 		}
 	}
 	render() {
-		//Shows navigation
+		//Shows navigation and changes icon "burger" to "x"
 		const showNavigation = (event) => {
 			const showNav = 'navShow';
+			const hideburger = 'burgerContainerHidden'
+			const showx = 'xIconShown'
 
-			return(this.setState({classname: showNav}));
+			return(this.setState({navclass: showNav, burgericonclass: hideburger, xiconclass: showx}));
 		}
-		//Hides navigation
+		//Hides navigation and changes icon "x" to "burger"
 		const hideNavigation = (event) => {
 			const hideNav = 'navHide';
+			const showburger = 'burgerContainerShown'
+			const hidex = 'xIconHidden'
 
-			return(this.setState({classname: hideNav}));
+			return(this.setState({navclass: hideNav, burgericonclass: showburger, xiconclass: hidex}));
 		}
 		return(
 			<div className="Navbar">
@@ -31,12 +37,17 @@ class Navbar extends React.Component {
 					<img src={require("../../Images/logomin.png")}/>
 				</div>
 
-				<div className='burgerContainer'>
-					<div className='burger topBun'></div>
-					<div className='burger meat'></div>
-					<div className='burger bottomBun'></div>
+				<div className= {this.state.burgericonclass} onClick={showNavigation}>
+					<div className='burger'></div>
+					<div className='burger'></div>
+					<div className='burger'></div>
 				</div>
-				<nav>
+
+				<div className= {this.state.xiconclass} onClick={hideNavigation}>
+					<div className='xBoth xFirst'></div>
+					<div className='xBoth xSecond'></div>
+				</div>
+				<nav className= {this.state.navclass} >
 					<Link activeClass="active" smooth spy to="appointment">
 					Umów się
 					</Link>
