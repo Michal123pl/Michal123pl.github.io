b743f6ecb80857451d1e166c65091bc669336833
Michal <michalnow777@interia.pl>
2023-01-24 21:47:18 +0100

    added honeypot to form and optimize photos a little

--- src/Elements/Contact/ContactForm.js
+++ src/Elements/Contact/ContactForm.js
@@ -12,7 +12,8 @@ const ContactForm = () => {
 	  email: '',
 	  phone: '',
 	  subject: '',
-	  message: ''
+	  message: '',
+	  country: ''
 	});
 
 	const sendEmail = event => {
@@ -29,103 +30,110 @@ const ContactForm = () => {
     		phone: state.phone,
     		email: state.email,
     		subject: state.subject,
-    		message: state.message
+    		message: state.message,
+    		country: state.country
     	})
     }).then( response => {
     	return response.json();
     })
     
   };
+
   const onInputChange = event => {
-    const { name, value } = event.target;
-    console.log(name + ' ' + value)
+		const { name, value } = event.target;
 
-   if (name == "phone")  {					//Checking is proper sign in input "phone"
-   	if (!isNaN(value)) {						//
-   		console.log("true " + value)	//
-   		setState({										//
-   		...state,											//
-   			[name]: value								//
-   		})														//
-   	}else {													//
-   		console.log("not a number")		//
-   	}}else{													//
-   	setState({
-   		...state,
-   			[name]: value
-   		})}
-   };
-	
-
+		if (name === "phone")  {					//Checking is proper sign in input "phone"
+	   	if (!isNaN(value)) {						//
+	   		setState({										//
+	   		...state,											//
+	   			[name]: value})							//
+	   	}else{
+	   		console.log('not a number')
+	   	}
+   	}else{													//
+	   	setState({
+	   		...state,
+	   			[name]: value})
+ 		}
+	}
 
 	return (
-		<form id='booking' onSubmit={sendEmail} autocomplete="off">
+		<form id='booking' onSubmit={sendEmail} autoComplete="off">
 			<label name="title" id="bookingTitle">Napisz do nas!</label>
+			<div id="country">
+				<input 
+          type="text"
+          name="country"
+          value={state.country}
+          placeholder="Zostaw puste"
+          onChange={onInputChange}
+          maxLength="30"
+        />
+      </div>
+
 			<div id="name">
-	          <input 
-	          	required
-	            type="text"
-	            name="name"
-	            value={state.name}
-	            placeholder="Podaj imię *"
-	            onChange={onInputChange}
-	            maxLength="30"
+        <input 
+        	required
+          type="text"
+          name="name"
+          value={state.name}
+          placeholder="*Podaj imię"
+          onChange={onInputChange}
+          maxLength="30"
+				/>
+        </div>
 
-	          />
-	        </div>
+        <div id="phone">
+          <input 
+            type="text"
+            name="phone"
+            value={state.phone}
+            placeholder="Wpisz swój numer: 123456789"
+            onChange={onInputChange}
+            maxLength="9"
 
-	        <div id="phone">
-	          <input 
-	            type="tel"
-	            name="phone"
-	            value={state.phone}
-	            placeholder="Wpisz swój numer: 123456789"
-	            onChange={onInputChange}
-	            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-	            maxLength="9"
+          />
+	      </div>
 
-	          />
-	        </div>
+        <div id="mail">
+          <input 
+          	required
+            type="mail"
+            name="email"
+            value={state.email}
+            placeholder="*Wpisz swój mail"
+            onChange={onInputChange}
+          />
+        </div>
 
-	        <div id="mail">
-	          <input 
-	          	required
-	            type="mail"
-	            name="email"
-	            value={state.email}
-	            placeholder="Wpisz swój mail *"
-	            onChange={onInputChange}
-	          />
-	        </div>
+        <div id="subject">
+          <input 
+          	required
+            type="text"
+            name="subject"
+            value={state.subject}
+            placeholder="Tytuł *"
+            onChange={onInputChange}
+            maxLength="50"
+          />
+        </div>
 
-	        <div id="subject">
-	          <input 
-	          	required
-	            type="text"
-	            name="subject"
-	            value={state.subject}
-	            placeholder="Tytuł *"
-	            onChange={onInputChange}
-	            maxLength="50"
-	          />
-	        </div>
+        <div id="message">
+          <textarea
+          	required
+          	id="bookingTextArea"
+          	form="booking"
+          	maxLength="300" 
+            name="message"
+            value={state.message}
+            placeholder="*Wiadomość"
+            onChange={onInputChange}
+          />
+        </div>
 
-	        <div id="message">
-	          <textarea
-	          	required
-	          	id="bookingTextArea"
-	          	form="booking"
-	          	maxLength="300" 
-	            name="message"
-	            value={state.message}
-	            placeholder="Wiadomość *"
-	            onChange={onInputChange}
-	          />
-	        </div>
-
-	        <div id="bookingSubmit" >
-	        	<button id="bookingSubmitButton"name="submit" type="submit">Wyślij</button>
-	        </div>
+        <div id="bookingSubmit" >
+        	<button id="bookingSubmitButton"name="submit" type="submit">Wyślij</button>
+        </div>
 		</form>
 	);
 }
